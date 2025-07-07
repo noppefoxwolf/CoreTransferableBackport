0d89bd520fd92db65552a5e214805155de5f5674
noppe <noppelabs@gmail.com>
2025-07-08 01:27:41 +0900

    Update Compatible.swift

--- Sources/CoreTransferableBackport/Compatible.swift
+++ Sources/CoreTransferableBackport/Compatible.swift
@@ -9,7 +9,7 @@ public final class Compatible<T: Transferable & Sendable>: Sendable {
     }
     
     public func exported(as contentType: UTType?) async throws -> Data {
-        if #available(iOS 18.2, *) {
+        if #available(iOS 18.2, macOS 15.2, *) {
             try await base.exported(as: contentType)
         } else {
             try await base.backport.exported(as: contentType)
@@ -20,7 +20,7 @@ public final class Compatible<T: Transferable & Sendable>: Sendable {
         to destinationDirectory: URL,
         contentType: UTType?
     ) async throws -> URL {
-        if #available(iOS 18.2, *) {
+        if #available(iOS 18.2, macOS 15.2, *) {
             try await base.export(to: destinationDirectory, contentType: contentType)
         } else {
             try await base.backport.export(to: destinationDirectory, contentType: contentType)
@@ -31,7 +31,7 @@ public final class Compatible<T: Transferable & Sendable>: Sendable {
         importing data: Data,
         contentType: UTType?
     ) async throws -> T {
-        if #available(iOS 18.2, *) {
+        if #available(iOS 18.2, macOS 15.2, *) {
             try await T.init(importing: data, contentType: contentType)
         } else {
             try await T.backport.`init`(importing: data, contentType: contentType)
@@ -42,7 +42,7 @@ public final class Compatible<T: Transferable & Sendable>: Sendable {
         importing url: URL,
         contentType: UTType?
     ) async throws -> T {
-        if #available(iOS 18.2, *) {
+        if #available(iOS 18.2, macOS 15.2, *) {
             try await T.init(importing: url, contentType: contentType)
         } else {
             try await T.backport.`init`(importing: url, contentType: contentType)
@@ -54,7 +54,7 @@ public final class Compatible<T: Transferable & Sendable>: Sendable {
     }
     
     public func exportedContentTypes() -> [UTType] {
-        if #available(iOS 18.2, *) {
+        if #available(iOS 18.2, macOS 15.2, *) {
             base.exportedContentTypes()
         } else {
             base.backport.exportedContentTypes()
@@ -70,7 +70,7 @@ public final class Compatible<T: Transferable & Sendable>: Sendable {
     }
     
     public var suggestedFilename: String? {
-        if #available(iOS 18.2, *) {
+        if #available(iOS 18.2, macOS 15.2, *) {
             base.suggestedFilename
         } else {
             base.backport.suggestedFilename
